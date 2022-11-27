b01840eb686763da216414596b4a9cacaa96a4ee
Bloom <bloomtauta@gmail.com>
2022-11-27 11:09:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+// function expectscuberGreetingForFeet() {}
+
+
+let feet = 199
+function scuberGreetingForFeet(feet){
+   if (feet <= 2500){
+    if (feet <= 400){
+    
+   return "This one is on me!";
+    }
+  else if (feet > 2000){
+    return "I will gladly take your thirty bucks.";
+  }
+}
+else 
+{
+  return "No can do.";
+}
+   
+}       
+
+    
+  
+
+let city = NYC
+function ternaryCheckCity(city){
   // Write your code here!
+   return  city === 'NYC' ? 'Ok, sounds good.' : "No go."
+  
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-}
+switch(tip){
+case 'generous': return "Thank you so much."
+   break;
+ 
+case 'not as generous': return "Thank you.";
+  break;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  default:
+    return "Bye."
+  }
 }
