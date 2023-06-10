f3815530f31f60a5498e3f3724b9b23a5a1f52d0
Bloom <bloomtauta@gmail.com>
2023-06-10 13:10:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
-// function expectscuberGreetingForFeet() {}
-
-
 let feet = 199
-function scuberGreetingForFeet(feet){
-   if (feet <= 2500){
-    if (feet <= 400){
-    
-   return "This one is on me!";
+function scuberGreetingForFeet(feet) {
+    if (feet <= 2500) {
+        if (feet <= 400) {
+            return "This one is on me!"
+        } else if (feet >= 2000) {
+            return "I will gladly take your thirty bucks."
+        }
+    } else {
+        return "No can do."
     }
-  else if (feet > 2000){
-    return "I will gladly take your thirty bucks.";
-  }
-}
-else 
-{
-  return "No can do.";
-}
-   
-}       
-
     
-  
-
-let city = NYC
-function ternaryCheckCity(city){
-  // Write your code here!
-   return  city === 'NYC' ? 'Ok, sounds good.' : "No go."
-  
+    
 }
+scuberGreetingForFeet();
+
+let city = "NYC"
+function ternaryCheckCity(city) {
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+ternaryCheckCity();
 
 function switchOnCharmFromTip(tip) {
-  // Write your code here!
-switch(tip){
-case 'generous': return "Thank you so much."
-   break;
- 
-case 'not as generous': return "Thank you.";
-  break;
-
-  default:
-    return "Bye."
-  }
+    switch (tip) {
+        case 'generous': return 'Thank you so much.';
+        break;
+        case 'not as generous' : return 'Thank you.';
+        break;
+        default:
+            return 'Bye.' 
+    }
 }
+switchOnCharmFromTip();
